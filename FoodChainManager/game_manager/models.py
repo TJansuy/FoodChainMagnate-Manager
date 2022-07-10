49f0b8c5f4be1d296658cfc807f6fe44eb476aa7
Trixi Jansuy <jansuyg@unlv.nevada.edu>
2022-07-10 13:59:00 -0700

    Adding configuration for game_manager app to separate the project from the base install

--- FoodChainManager/game_manager/models.py
+++ FoodChainManager/game_manager/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class GameSession(models.Model):
+  session_id = models.IntegerField(primary_key=True) # Force the session_id to be unique
+
+
+
+class GameSessionPlayers(models.Model):
+  session_id = models.ForeignKey(GameSession, on_delete=models.CASCADE) # If we delete the session, delete this as well
+  
+
+
+class GameSessionHistory(models.Model):
+  session_id = models.ForeignKey(GameSession, on_delete=models.CASCADE) # If we delete the session, delete this as well
+
