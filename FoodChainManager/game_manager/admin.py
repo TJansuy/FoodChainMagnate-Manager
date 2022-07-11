9023487eff846f3afda383bd3f358e5994f8c76c
Trixi Jansuy <jansuyg@unlv.nevada.edu>
2022-07-10 19:31:05 -0700

    Adding support in the admin panel for the game_session entries

--- FoodChainManager/game_manager/admin.py
+++ FoodChainManager/game_manager/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import GameSession, GameSessionPlayer, GameSessionHistory
+
 # Register your models here.
+
+class PlayersInline(admin.StackedInline):
+    model = GameSessionPlayer
+    extra = 0
+class SessionInline(admin.StackedInline):
+    model = GameSessionHistory
+class SessionAdmin(admin.ModelAdmin):
+
+    inlines = [PlayersInline, SessionInline]
+
+admin.site.register(GameSession, SessionAdmin)
--- FoodChainManager/game_manager/models.py
+++ FoodChainManager/game_manager/models.py
@@ -2,16 +2,23 @@ from django.db import models
 
 # Create your models here.
 class GameSession(models.Model):
-  session_id = models.IntegerField(primary_key=True) # Force the session_id to be unique
-  game_open = models.BooleanField(default=False)
-  
+    session_id = models.IntegerField(primary_key=True, default=1, unique=True, null=False) # Force the session_id to be unique
+    game_open = models.BooleanField(default=False) # Allow game to be seen
+    
 
 
-class GameSessionPlayers(models.Model):
-  session_id = models.ForeignKey(GameSession, on_delete=models.CASCADE) # If we delete the session, delete this as well
-  
+class GameSessionPlayer(models.Model):
+    session_id = models.ForeignKey(GameSession, on_delete=models.CASCADE, default=0) # If we delete the session, delete this as well
+    player_id = models.IntegerField(unique=True, default=1) # Unique identifier for the player
+    player_name = models.CharField(max_length=200) # Representable name for that player
 
+    def __str__(self):
+        return self.player_name
 
 class GameSessionHistory(models.Model):
-  session_id = models.ForeignKey(GameSession, on_delete=models.CASCADE) # If we delete the session, delete this as well
-
+    session_id = models.ForeignKey(GameSession, on_delete=models.CASCADE, default=0) # If we delete the session, delete this as well
+    turn_id = models.PositiveIntegerField(default=1) # What turn this event took on
+    player_id = models.ForeignKey(GameSessionPlayer, on_delete=models.RESTRICT, default=None) # The player that performed this event
+    
+    def __str__(self):
+        return "Turn: " + self.turn_id + " by " + self.player_id
