710a17a4fc9bbd6e85eca4d2d5ff915c37a7bc35
Trixi Jansuy <jansuyg@unlv.nevada.edu>
2022-07-10 19:48:32 -0700

    Adding the ability for open game sessions to be seen from the game_manager index

--- FoodChainManager/game_manager/views.py
+++ FoodChainManager/game_manager/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, HttpResponse
 
+from .models import GameSession, GameSessionPlayer, GameSessionHistory
+
 # Create your views here.
 def index(request):
-    return render(request, 'game_manager/index.html', {})
+    sessions = GameSession.objects.filter(game_open=True)
+
+    return render(request, 'game_manager/index.html', {'sessions': sessions})
 
     
